b524eb3e075dbb1d5acbd5f1fa56032533bbf23d
danielv-exp <daniel.valverde@expertiatravel.com>
2026-06-06 17:42:42 -0500

    ttc(d211ea34): interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,14 @@
-export interface UserResponse {
+interface UserResponse {
   id: string;
   email: string;
   name: string | null;
   github_id: string | null;
   has_github: boolean;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
 }
